e92a8bbc09ce580330be1180ccc067462c816892
msnthr0pe <pavlid66@gmail.com>
2024-05-30 21:41:07 +0300

    partial localization?

--- app/src/main/java/com/example/kurs/Activity.java
+++ app/src/main/java/com/example/kurs/Activity.java
@@ -41,6 +41,7 @@ public class Activity extends AppCompatActivity implements NavigationView.OnNavi
 
         navigationView.setNavigationItemSelectedListener(this);
 
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
                 R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
